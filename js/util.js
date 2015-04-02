efaf7d5b0cf2b58946cc17b0e1d31b4dad04e185
Drago Rosson <drago@rosson.me>
2015-04-02 16:01:44 -0500

    Update endpoints list

--- js/util.js
+++ js/util.js
@@ -16,12 +16,12 @@
 var HotUI = {},
     BaseObject = Object.create(null),
     ENDPOINTS = {
-        templateValidate: "/api/hotui/template_validate/",
-        jsonToYAML: "/api/hotui/json_to_yaml/",
-        yamlToJSON: "/api/hotui/yaml_to_json/",
-        urlToJSON: "/api/hotui/url_to_json/",
-        downloadTemplate: "/api/hotui/download_template/",
-        resourceTypeShow: "/api/hotui/resource_type_show/",
+        templateValidate: HOTUI_API_ROOT + "template_validate/",
+        jsonToYAML: HOTUI_API_ROOT + "json_to_yaml/",
+        yamlToJSON: HOTUI_API_ROOT + "yaml_to_json/",
+        urlToJSON: HOTUI_API_ROOT + "url_to_json/",
+        downloadTemplate: HOTUI_API_ROOT + "download_template/",
+        resourceTypeShow: HOTUI_API_ROOT + "resource_type_show/"
     };
 
 Object.defineProperty(BaseObject, 'extend', {
